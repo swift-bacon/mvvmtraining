dac3ea9705b577ce32737f058bf36af873311676
Marcin Pietrzak <pietrzakmarcin@hotmail.com>
2025-08-07 19:02:33 +0200

    init project

---
+++ mvvmtraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvmtraining
+//
+//  Created by Unit27 on 07/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mvvmtrainingTests/mvvmtrainingTests.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmtrainingTests.swift
+//  mvvmtrainingTests
+//
+//  Created by Unit27 on 07/08/2025.
+//
+
+import Testing
+@testable import mvvmtraining
+
+struct mvvmtrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
